e2822bdc6015fda5dec901b03d8dfecf262f693e
Irem Subası <irem.subasi@rapsodo.com>
2023-06-16 16:19:38 +0300

    Initial Commit

---
+++ PagePal/PagePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagePalApp.swift
+//  PagePal
+//
+//  Created by Irem Subası on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PagePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
